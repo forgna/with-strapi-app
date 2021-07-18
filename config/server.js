fcbf6ed8a9fdb62bc64297ad0afc982e8557c6f3
Rathkalyfogna <forgna@wonderpass.asia>
2021-07-18 17:57:03 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c9e8e3552e75b25914c054df208f2767'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'da1c7497-8465-4758-a546-7d756233c02d'
+};
