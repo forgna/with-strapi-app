2c6782a4d81caf1822231da190b0a76ac92be165
Rathkalyfogna <forgna@wonderpass.asia>
2021-07-18 22:35:17 +0700

    Add slugify

---
+++ api/career/models/career.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+const slugify = require("slugify");
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.title) {
+        data.slug = slugify(data.title, { lower: true });
+      }
+    },
+  },
+};
